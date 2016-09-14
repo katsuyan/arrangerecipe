df5541ecc4458ab700a14ec97f47aa784007da12
tajima12 <zakichan1216@gmail.com>
2016-09-15 00:33:03 +0900

    add setting

--- arrangerecipe/settings.py
+++ arrangerecipe/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gunicorn'
+    'gunicorn',
+    'recipe'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -110,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
