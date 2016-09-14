8d099536dff3b090ce66671372f7a1cbb95cc2e1
tajima12 <zakichan1216@gmail.com>
2016-09-15 01:26:49 +0900

    add root page

--- arrangerecipe/settings.py
+++ arrangerecipe/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    #os.path.join(BASE_DIR, 'static'),
+    #os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
