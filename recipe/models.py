a216578034c0ebcf800ceb65bf770eac67f3504b
tajima12 <zakichan1216@gmail.com>
2016-09-15 11:44:40 +0900

    add model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Taste(models.Model):
+    """味"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('味付け', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """料理のカテゴリー"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('味付け', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class recipe(models.Model):
+    """レシピ"""
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, verbose_name='カテゴリー', related_name='recipe')
+    taste = models.ForeignKey(Taste, verbose_name='味付け', related_name='recipe')
+
+    def __str__(self):
+        return self.taste.name
