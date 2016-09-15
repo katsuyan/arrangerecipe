07079192f1c7af5f258d5e862d457b059c8f3769
tajima12 <zakichan1216@gmail.com>
2016-09-15 17:45:14 +0900

    add to change radio button

--- recipe/templates/js/index.js
+++ recipe/templates/js/index.js
@@ -2,18 +2,29 @@ var ARRANGE = ARRANGE || {};
 
 (function () {
 
+  $.sygImageRadio( '.radioGroup1' );
+  $.sygImageRadio( '.checkGroup1' );
+
   $('#index').on('click', '#cook_button', function(e) {
-    var category_id = $('input[name=category]:checked').val()
-    var taste_id = $('input[name=taste]:checked').val()
+    var category_id = $('input[name=category]:checked').val();
+    if(!category_id) {
+      alert('カテゴリーを選択してください');
+      return false;
+    }
+    var taste_id = $('input[name=taste]:checked').val();
+    if(!taste_id) {
+      alert('味付けを選択してください');
+      return false;
+    }
     $.ajax({
       type: 'GET',
       url: '{% url "recipeid" %}',
       data: {category_id: category_id, taste_id: taste_id},
       success: function(data) {
-        window.open('http://cookpad.com/recipe/' + data.id)
+        window.open('http://cookpad.com/recipe/' + data.id);
       },
       error: function(data) {
-        console.log(data)
+        console.log(data);
         alert("失敗");
       }
     });
