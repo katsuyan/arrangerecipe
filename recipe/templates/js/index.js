4bf6ffeb911453e5278e18dc098e13d2f8eb0c14
tajima12 <zakichan1216@gmail.com>
2016-09-15 15:08:00 +0900

    add data to html

--- recipe/templates/js/index.js
+++ recipe/templates/js/index.js
@@ -2,7 +2,7 @@ var ARRANGE = ARRANGE || {};
 
 (function () {
 
-  $('#index').on('click', '#cook_button', function(e){
+  $('#index').on('click', '#cook_button', function(e) {
     window.open('http://cookpad.com/recipe/3141186')
   });
 
