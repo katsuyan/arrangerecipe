d2f0d344b49f0929a7096d99a557dc5bca2964f1
tajima12 <zakichan1216@gmail.com>
2016-09-15 14:08:38 +0900

    add get tastes and categories

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.db import transaction
+from recipe.models import Taste, Category, Recipe
+from django.views.decorators.csrf import ensure_csrf_cookie
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def book_list(request):
+    tastes = Taste.objects.all().order_by("id")
+    categories = Category.objects.all().order_by("id")
     return render(request, 'recipe/index.html')
