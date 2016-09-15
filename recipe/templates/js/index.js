c0613db8dfe4d0e4c061e2305bfdb53c69863c49
tajima12 <zakichan1216@gmail.com>
2016-09-15 15:21:54 +0900

    add to get checked id

--- recipe/templates/js/index.js
+++ recipe/templates/js/index.js
@@ -3,7 +3,10 @@ var ARRANGE = ARRANGE || {};
 (function () {
 
   $('#index').on('click', '#cook_button', function(e) {
-    window.open('http://cookpad.com/recipe/3141186')
+    //window.open('http://cookpad.com/recipe/3141186')
+
+    console.log($('input[name=category]:checked').val())
+    console.log($('input[name=taste]:checked').val())
   });
 
 }());
